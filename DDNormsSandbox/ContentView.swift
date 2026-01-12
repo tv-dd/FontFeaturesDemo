c26ca3c2d492d2a71e5beb41a7e38a1994bb73c4
tv-dd <tim.van@doordash.com>
2026-01-12 10:56:44 -0500

    Initial Commit

---
+++ DDNormsSandbox/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Copyright © 2026 DoorDash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDNormsSandbox/DDNormsSandboxApp.swift
@@ -0,0 +1,15 @@
+//
+//  DDNormsSandboxApp.swift
+//  Copyright © 2026 DoorDash. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DDNormsSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
